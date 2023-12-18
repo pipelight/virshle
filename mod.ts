9e203acf519d85aa95f8c0222e11a95372e863a7
areskill <areskul@areskul.com>
2023-12-18 21:55:20 +0100

    add tmp file auto deletion for convenience

--- mod.ts
+++ mod.ts
@@ -77,6 +77,9 @@ cli
     // Sub-process
     const cmd = new Deno.Command("virsh", { args: [command, tmp.file] });
     const child = cmd.spawn();
+
+    // Clean up
+    await Deno.remove(tmp.file);
   });
 
 await cli.parse(Deno.args);
