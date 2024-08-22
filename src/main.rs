1c421f990eb1df4d68559fb8af4895b2eb6c1c10
areskill <areskul@areskul.com>
2024-08-22 20:05:51 +0200

    complete xml conversion

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,19 @@ pub fn to_xml(value: &Value) -> Result<String> {
     Ok(res)
 }
 
-pub fn get_attributes(map: &mut Map<String, Value>) -> Result<Vec<(String, String)>> {
+pub fn get_text(map: &mut Map<String, Value>) -> Result<Option<String>> {
+    let prefix = "#";
+    let mut text: Option<String> = None;
+    for key in map.clone().keys() {
+        if key.starts_with(prefix) {
+            let value = map.get(key).unwrap();
+            text = Some(value.to_string().trim_matches('"').to_owned());
+        }
+    }
+    Ok(text)
+}
+
+pub fn get_attributes(map: &mut Map<String, Value>) -> Result<Option<Vec<(String, String)>>> {
     let prefix = "@";
     let mut attributes: Vec<(String, String)> = vec![];
 
@@ -45,73 +57,83 @@ pub fn get_attributes(map: &mut Map<String, Value>) -> Result<Vec<(String, Strin
         if key.starts_with(prefix) {
             let attribute: (String, String) = (
                 key.strip_prefix(prefix).unwrap().to_owned(),
-                map.get(key).unwrap().as_str().unwrap().to_owned(),
+                map.get(key).unwrap().to_string(),
             );
             map.shift_remove_entry(key);
             attributes.push(attribute);
         }
     }
-    Ok(attributes)
+    if attributes.is_empty() {
+        Ok(None)
+    } else {
+        Ok(Some(attributes))
+    }
 }
 
-pub fn print_open_tag(name: &str, attributes: &Vec<(String, String)>) -> Result<()> {
-    let attributes = attributes
-        .iter()
-        // Separate attributes with a space
-        .map(|(k, v)| format!(" {k}=\"{v}\""))
-        .collect::<Vec<String>>()
-        .join("");
+pub fn print_open_tag(
+    name: &str,
+    attributes: Option<Vec<(String, String)>>,
+    text: Option<String>,
+    indent_level: &mut i64,
+) -> Result<()> {
+    let ident = "  ".repeat(*indent_level as usize);
+    let mut open_tag = format!("{ident}<{name}");
 
-    let open_tag = format!("<{name}") + &attributes + ">";
+    if let Some(attributes) = attributes {
+        let attributes = attributes
+            .iter()
+            // Separate attributes with a space
+            .map(|(k, v)| format!(" {k}={v}"))
+            .collect::<Vec<String>>()
+            .join("");
+        open_tag += &attributes;
+    }
+
+    open_tag += ">";
+
+    if let Some(text) = text {
+        open_tag += &format!("{}", text);
+    }
+
     println!("{}", open_tag);
     Ok(())
 }
 
-pub fn print_close_tag(name: &str) -> Result<()> {
-    let close_tag = format!("</{name}>");
+pub fn print_close_tag(name: &str, ident_level: &mut i64) -> Result<()> {
+    let ident = "  ".repeat(*ident_level as usize);
+    let close_tag = format!("{ident}</{name}>");
     println!("{}", close_tag);
     Ok(())
-}
-
-pub fn get_text(map: &mut Map<String, Value>) -> Result<Option<String>> {
-    let prefix = "#";
-    let mut text: Option<String> = None;
-    for key in map.clone().keys() {
-        if key.starts_with(prefix) {
-            text = Some(map.get(key).unwrap().as_str().unwrap().to_owned());
-        }
-    }
-    Ok(text)
 }
 
 // pub fn get_element(map: &mut Map<String, Value>) -> Result<(String,Value)> {
 //     Ok(())
 // }
 
-pub fn read_value(value: &mut Value) -> Result<()> {
+pub fn read_value(key: &str, value: &mut Value, ident_level: &mut i64) -> Result<()> {
     match value {
         Value::Object(map) => {
-            for (key, value) in map {
-                if value.is_object() {
-                    let attributes = get_attributes(value.as_object_mut().unwrap())?;
-                    let text = get_text(value.as_object_mut().unwrap())?;
+            let mut ident_level: i64 = *ident_level + 1;
+            let text = get_text(map)?;
+            let attributes = get_attributes(map)?;
 
-                    print_open_tag(key, &attributes)?;
-
-                    // if it is an object, check recursively
-                    read_value(value)?;
-
-                    print_close_tag(key)?;
-                } else {
-                    println!("<{}>: {}", key, value);
-                }
+            print_open_tag(key, attributes, text, &mut ident_level)?;
+            for (k, v) in map {
+                read_value(k, v, &mut ident_level)?;
+            }
+            print_close_tag(key, &mut ident_level)?;
+        }
+        Value::String(value) => {
+            print_open_tag(key, None, None, ident_level)?;
+            let ident = "  ".repeat(*ident_level as usize);
+            println!("{ident}{value}");
+            print_close_tag(key, ident_level)?;
+        }
+        Value::Array(value) => {
+            for e in value {
+                read_value(key, e, ident_level)?;
             }
         }
-        Value::String(string) => {}
-        Value::Number(number) => {}
-        Value::Array(array) => {}
-        Value::Bool(e) => {}
-        Value::Null => {}
         _ => {}
     }
     Ok(())
@@ -141,13 +163,13 @@ mod test {
 
         let string = fs::read_to_string(path).into_diagnostic()?;
 
-        let res = from_toml(&string)?;
-        println!("{:#?}", res);
-        // assert!(res.is_ok());
+        let res = from_toml(&string);
+        // println!("{:#?}", res);
+        assert!(res.is_ok());
 
         Ok(())
     }
-    // #[test]
+    #[test]
     fn from_toml_to_xml() -> Result<()> {
         let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         path.push("examples/vm/base.toml");
@@ -155,14 +177,17 @@ mod test {
 
         let string = fs::read_to_string(path).into_diagnostic()?;
 
-        let value = from_toml(&string)?;
+        let mut value = from_toml(&string)?;
+        println!("{:#?}", value);
 
-        let res = to_xml(&value)?;
+        println!("");
+        // let res = to_xml(&value)?;
 
+        read_value("root", &mut value, &mut 0)?;
         // assert!(res.is_ok());
         Ok(())
     }
-    #[test]
+    // #[test]
     fn value_to_xml() -> Result<()> {
         let mut value = json!({
             "domain": {
@@ -178,7 +203,7 @@ mod test {
         });
 
         println!("");
-        read_value(&mut value)?;
+        read_value("root", &mut value, &mut 0)?;
 
         // let res = from_toml(&string);
         // assert!(res.is_ok());
